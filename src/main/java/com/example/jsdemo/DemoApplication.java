64bdd14b520b1640d8de7f191e0686cf108bbf55
Dave Syer <dsyer@vmware.com>
2023-08-02 14:21:35 +0100

    Webflux version

--- src/main/java/com/example/jsdemo/DemoApplication.java
+++ src/main/java/com/example/jsdemo/DemoApplication.java
@@ -5,31 +5,19 @@ import java.time.Duration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import org.springframework.web.bind.annotation.RestController;
 
 import reactor.core.publisher.Flux;
 
 @SpringBootApplication
-@Controller
+@RestController
 public class DemoApplication {
 
 	@GetMapping(path = "/", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-	public SseEmitter stream() {
-		SseEmitter emitter = new SseEmitter();
-		Flux.interval(Duration.ofSeconds(5)).subscribe(
-				value -> {
-					try {
-						String text = render(value);
-						// https://github.com/spring-projects/spring-framework/issues/30965
-						emitter.send(text.replace("\n", "\ndata:"), MediaType.TEXT_HTML);
-					} catch (Exception e) {
-						emitter.completeWithError(e);
-					}
-				}, emitter::completeWithError, emitter::complete);
-		;
-		return emitter;
+	public Flux<String> stream() {
+		return Flux.interval(Duration.ofSeconds(5)).map(
+				value -> render(value));
 	}
 
 	private String render(Long value) {
